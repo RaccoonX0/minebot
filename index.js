f13bebc87492484e5dc01c6858b05e6456925d01
RaccoonX0 <147911341+RaccoonX0@users.noreply.github.com>
2025-01-17 22:51:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import {readFileSync, writeFileSync} from "fs";
 import {startBans} from "./bot/ban/ban.js";
 import {MWBot} from "./bot/masedworld.js";
 import {randInt} from "./config/func.js";
-import {CMBot} from "./bot/cheatmine.js";
-import {MBBot} from "./bot/mineblaze.js";
 import {TBot} from "./tbot/bot.js";
 
 export let playerDeaths;
@@ -21,22 +19,6 @@ export const botsObjData = {
     "s7": () => { return startBotMW({nickname: "AntiKemper1ng", portal: "s7"}) },
     "s8": () => { return startBotMW({nickname: "Temper1ng", portal: "s8"}) },
   },
-  "cheatmine": {
-    "s1": () => { return startBotCM({nickname: "Tramp2024", portal: "s1"}) },
-    "s2": () => { return startBotCM({nickname: "Ryfkin228", portal: "s2"}) },
-  },
-  "mineblaze": {
-    "s1": () => { return startBotMB({nickname: "lohkgwg1", portal: "s1"}) }, // lohkgwg1 s1
-    "s2": () => { return startBotMB({nickname: "TARAKAN2149", portal: "s2"}) }, // TARAKAN2149 s2
-    "s3": () => { return startBotMB({nickname: "Vkabababa", portal: "s3"}) }, // Vkabababa s3
-    "s4": () => { return startBotMB({nickname: "__IRISHKA__", portal: "s4"}) }, // __IRISHKA__ s4
-    "s5": () => { return startBotMB({nickname: "TYTSFS", portal: "s5"}) }, // TYTSFS s5
-    "s6": () => { return startBotMB({}) }, // s6
-    "s7": () => { return startBotMB({nickname: "KoTiK_B_KeDaX", portal: "s7"}) }, // KoTiK_B_KeDaX s7
-    "s8": () => { return startBotMB({}) }, // s8
-    "s9": () => { return startBotMB({}) }, // s9
-    "s10": () => { return startBotMB({}) }, // s10
-  },
 };
 export let botsObj = {
   "masedworld": {
@@ -48,22 +30,6 @@ export let botsObj = {
     "s6": null,
     "s7": null,
     "s8": null,
-  },
-  "cheatmine": {
-    "s1": null,
-    "s2": null,
-  },
-  "mineblaze": {
-    "s1": null,
-    "s2": null,
-    "s3": null,
-    "s4": null,
-    "s5": null,
-    "s6": null,
-    "s7": null,
-    "s8": null,
-    "s9": null,
-    "s10": null,
   },
 };
 loadBlacklist();
